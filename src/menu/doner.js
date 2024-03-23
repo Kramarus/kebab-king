b3b832b66e408ebf0d358814bf27dd1b82336a15
Kramarus <60113906+Kramarus@users.noreply.github.com>
2024-03-23 10:12:55 +0100

    Update doner.js
    Preiseangepasst

--- src/menu/doner.js
+++ src/menu/doner.js
@@ -6,63 +6,63 @@ const donerList = [
         category: "Döner",
         name: "Döner Tasche",
         comment: "mit gem. Salat & Sauce",
-        price: 7.0
+        price: 7.5
     },
     {
         id: '2',
         category: "Döner",
         name: "Mega-Döner",
         comment: "(extra Fleisch) mit gem. Salat & Sauce",
-        price: 8.0
+        price: 8.5
     },
     {
         id: '3',
         category: "Döner",
         name: "Dürüm Döner",
         comment: "(gerollt) mit gem. Salat & Sauce",
-        price: 7.5
+        price: 8.0
     },
     {
         id: '4',
         category: "Döner",
         name: "Mega-Rollo",
         comment: "(extra Fleisch) mit gem. Salat & Sauce",
-        price: 8.5
+        price: 9.0
     },
     {
         id: '5',
         category: "Döner",
         name: "Döner-Box",
         comment: "(Pomm-Döner) mit Pommes, Dönerfleisch & Sauce",
-        price: 7.0
+        price: 8.0
     },
     {
         id: '6',
         category: "Döner",
         name: "Döner-Teller",
         comment: "mit Pommes, Dönerfleisch & Sauce",
-        price: 9.5
+        price: 11.00
     },
     {
         id: '7',
         category: "Döner",
         name: "Döner-Teller",
         comment: "mit gem. Salat, Dönerfleisch & Sauce",
-        price: 9.5
+        price: 11.00
     },
     {
         id: '8',
         category: "Döner",
         name: "Döner-Teller (komplett)",
         comment: "mit Pommes gem. Salat & Sauce",
-        price: 10.5
+        price: 12.00
     },
     {
         id: '9',
         category: "Döner",
         name: "Taxi-Teller",
         comment: "mit Dönerfleisch, Currywurst, Pommes & Sauce",
-        price: 10.5
+        price: 11.5
     },
     {
         id: '10',
